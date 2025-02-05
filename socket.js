1ba46e0f194193a258962b7e5589d60cb04d50ea
hamediseyedali <hamediseyedali@gmail.com>
2025-02-05 15:11:40 +0330

    socketManager implemented fully

--- socket.js
+++ socket.js
@@ -20,31 +20,81 @@ class SocketManager {
     this.io.on("connection", (socket) => {
       console.log("New socket:", socket.id);
 
-      //USER
       socket.on("create-user", (username) => {
         const user = User(username);
         console.log("New user:", user.id);
-        users[user.id] = user;
-        socket.emit("user-created");
+        this.users.set(user.id, user);
+        socket.emit("user-created"),
+          {
+            id: user.id,
+          };
       });
       socket.on("create-channel", (channelName, userId) => {
         const channel = Channel(channelName, this.users[userId]);
-        this.channels[channel.id] - channel;
+        this.channels.set(channel.id, channel);
         this.io.emit("channel-created", {
           id: channel.id,
         });
       });
       socket.on("join-channel", (channelId, userId) => {
         const channel = this.channels.get(channelId);
-      });
-      socket.on("leave-channel", (channelName, username) => {
-        this.io.emit("channel-leave", {
-          id: channel.id,
-          name: channelName,
+        const user = this.users.get(userId);
+        channel.addUser(user);
+        user.setChannel(channel);
+        socket.join(channelId);
+
+        this.io.to(channelId).emit("user-joined", {
+          userId: user.id,
+          username: user.name,
+          channelId: channel.id,
+          participants: channel.getUsers(),
         });
       });
-      socket.on("disconnect", () => {
-        console.log("socket disconnect");
+
+      socket.on("leave-channel", (channelName, username) => {
+        channel.removeUser(user.id);
+        user.clearChannel();
+        socket.leave(channelId);
+
+        this.io.to(channelId).emit("user-left", {
+          userId: user.id,
+          channelId: channel.id,
+          participants: channel.getUsers(),
+        });
+      });
+
+      socket.on("toggle-mute", (userId) => {
+        const user = this.users.get(userId);
+        if (user.isMuted()) {
+          user.unmuteUser();
+        } else {
+          user.muteUser();
+        }
+
+        this.io.to(user.currentChannel.id).emit("mute-toggle", {
+          userId: user.id,
+          isMuted: user.isMuted(),
+        });
+      });
+
+      socket.on("start-speaking", (userId) => {
+        const user = this.users.get(userId);
+        user.setSpeaking(true);
+
+        this.io.to(user.currentChannel.id).emit("speaking-started", {
+          userId: user.id,
+          username: user.name,
+        });
+      });
+
+      socket.on("stop-speaking", (userId) => {
+        const user = this.users.get(userId);
+        user.setSpeaking(false);
+
+        this.io.to(user.currentChannel.id).emit("speaking-stopped", {
+          userId: user.id,
+          username: user.name,
+        });
       });
     });
   }
