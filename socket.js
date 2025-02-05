5eb93aea3eae3b5114e622fb50356d81482b0dab
hamediseyedali <hamediseyedali@gmail.com>
2025-02-05 16:36:25 +0330

    socket.js completed

--- socket.js
+++ socket.js
@@ -1,5 +1,4 @@
 const { Server } = require("socket.io");
-
 const User = require("./models/user");
 const Channel = require("./models/channel");
 
@@ -16,64 +15,87 @@ class SocketManager {
     this.users = new Map();
     this.initalizeEvents();
   }
+
   initalizeEvents() {
     this.io.on("connection", (socket) => {
       console.log("New socket:", socket.id);
 
       socket.on("create-user", (username) => {
-        const user = User(username);
+        const user = new User(username);
         console.log("New user:", user.id);
         this.users.set(user.id, user);
-        socket.emit("user-created"),
-          {
-            id: user.id,
-          };
+        socket.emit("user-created", { user });
       });
+
       socket.on("create-channel", (channelName, userId) => {
-        const channel = Channel(channelName, this.users[userId]);
-        this.channels.set(channel.id, channel);
-        this.io.emit("channel-created", {
-          id: channel.id,
-        });
+        const user = this.users.get(userId);
+        const channel = new Channel(channelName, user);
+        this.channels.push(channel);
+        socket.emit("channel-created", { channel });
       });
+
       socket.on("join-channel", (channelId, userId) => {
-        const channel = this.channels.get(channelId);
+        const channel = this.channels.find((c) => c.id === channelId);
         const user = this.users.get(userId);
         channel.addUser(user);
         user.setChannel(channel);
         socket.join(channelId);
 
-        this.io.to(channelId).emit("user-joined", {
-          userId: user.id,
-          username: user.name,
-          channelId: channel.id,
-          participants: channel.getUsers(),
-        });
+        this.io.to(channelId).emit("user-joined", { user, channel });
       });
 
-      socket.on("leave-channel", (channelName, username) => {
-        channel.removeUser(user.id);
+      socket.on("leave-channel", (channelId, userId) => {
+        const channel = this.channels.find((c) => c.id === channelId);
+        const user = this.users.get(userId);
+        channel.removeUser(userId);
         user.clearChannel();
         socket.leave(channelId);
 
-        this.io.to(channelId).emit("user-left", {
-          userId: user.id,
-          channelId: channel.id,
-          participants: channel.getUsers(),
-        });
+        this.io.to(channelId).emit("user-left", { user, channel });
+      });
+
+      // --- WebRTC Signaling Events ---
+
+      socket.on("send-offer", (offer, targetUserId) => {
+        const targetUser = this.users.get(targetUserId);
+        if (
+          targetUser &&
+          targetUser.currentChannel === this.users.get(socket.id).currentChannel
+        ) {
+          socket.to(targetUserId).emit("receive-offer", offer, socket.id);
+        }
+      });
+
+      socket.on("send-answer", (answer, targetUserId) => {
+        const targetUser = this.users.get(targetUserId);
+        if (
+          targetUser &&
+          targetUser.currentChannel === this.users.get(socket.id).currentChannel
+        ) {
+          socket.to(targetUserId).emit("receive-answer", answer);
+        }
+      });
+
+      socket.on("send-ice-candidate", (candidate, targetUserId) => {
+        const targetUser = this.users.get(targetUserId);
+        if (
+          targetUser &&
+          targetUser.currentChannel === this.users.get(socket.id).currentChannel
+        ) {
+          socket.to(targetUserId).emit("receive-ice-candidate", candidate);
+        }
       });
 
       socket.on("toggle-mute", (userId) => {
         const user = this.users.get(userId);
-        if (user.isMuted()) {
+        if (user.isMuted) {
           user.unmuteUser();
         } else {
           user.muteUser();
         }
 
         this.io.to(user.currentChannel.id).emit("mute-toggle", {
-          userId: user.id,
-          isMuted: user.isMuted(),
+          user: user,
         });
       });
 
@@ -82,8 +104,7 @@ class SocketManager {
         user.setSpeaking(true);
 
         this.io.to(user.currentChannel.id).emit("speaking-started", {
-          userId: user.id,
-          username: user.name,
+          user: user,
         });
       });
 
@@ -92,11 +113,11 @@ class SocketManager {
         user.setSpeaking(false);
 
         this.io.to(user.currentChannel.id).emit("speaking-stopped", {
-          userId: user.id,
-          username: user.name,
+          user: user,
         });
       });
     });
   }
 }
+
 module.exports = SocketManager;
